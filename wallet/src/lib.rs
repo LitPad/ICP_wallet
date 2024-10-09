ba11ce3bf5f86b079f7077d3df5e4b5f660a9df9
Utee <utibeabasiekong2@gmail.com>
2024-10-09 23:37:06 +0100

    update: setup stable memory for canister

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -1,4 +1,6 @@
+use candid::CandidType;
 use ic_cdk_macros::*;
+use serde::Deserialize;
 use std::cell::RefCell;
 
 thread_local! {
@@ -10,15 +12,40 @@ struct RuntimeState {
     data: Data,
 }
 
-#[derive(Default)]
+#[derive(CandidType, Deserialize, Default)]
 struct Data {
     todos: Vec<TodoItem>,
 }
 
+#[derive(CandidType, Deserialize, Clone)]
 struct TodoItem {
     id: u32,
     done: bool,
     name: String,
+}
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    RUNTIME_STATE.with(|state| {
+        let data = &state.borrow().data;
+        ic_cdk::storage::stable_save((data,)).unwrap();
+    });
+    // instead of using unwrap, handle the result for prod
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    match ic_cdk::storage::stable_restore::<(Data,)>() {
+        Ok((data,)) => {
+            let runtime_state = RuntimeState { data };
+            RUNTIME_STATE.with(|state| *state.borrow_mut() = runtime_state);
+        }
+        Err(e) => {
+            ic_cdk::println!("Failed to restore stable memory: {:?}", e);
+
+            RUNTIME_STATE.with(|state| *state.borrow_mut() = RuntimeState::default());
+        }
+    }
 }
 
 #[update]
@@ -36,4 +63,13 @@ fn add_impl(name: String, runtime_state: &mut RuntimeState) -> u32 {
     });
 
     id
+}
+
+#[query]
+fn get() -> Vec<TodoItem> {
+    RUNTIME_STATE.with(|state| get_impl(&state.borrow_mut()))
+}
+
+fn get_impl(runtime_state: &RuntimeState) -> Vec<TodoItem> {
+    runtime_state.data.todos.clone()
 }
